0ad4929f1c49483984ae193894172008c0a90319
Abuzar Qureshi <abz95@msn.com>
2024-04-01 01:55:15 +0500

    updated AuthorizationTest.js

--- tests/authorizationTest.js
+++ tests/authorizationTest.js
@@ -5,17 +5,17 @@ const config = readConfigFile('../resources/configs/awsData.json');
 
 describe('API Gateway Authorization Tests', function() {
   
-  it('Should return 401 Forbidden if no authorizationToken header is provided', async function() {
+  it('Return 403 Forbidden if no authorizationToken header is provided', async function() {
     try {
         const response = await sendHttpRequest('GET', config.endPointURL);
-        expect(response.status).to.equal(401);
+        expect(response.status).to.equal(403);
     } 
     catch (error) {
         throw error;
     }
   });
 
-  it('Should return 401 Unauthorized for authorizationToken header with value "unauthorized"', async function() {
+  it('Return 401 Unauthorized for authorizationToken header with value "unauthorized"', async function() {
     try {
         headers = { authorizationToken: 'unauthorized' };
         const response = await sendHttpRequest('GET', config.endPointURL, headers);
@@ -26,9 +26,9 @@ describe('API Gateway Authorization Tests', function() {
     }
   });
 
-  it('Should return 403 Forbidden for authorizationToken header with value "Bearer deny"', async function() {
+  it('Return 403 Forbidden for authorizationToken header with value "Bearer deny"', async function() {
     try {
-        headers = { authorizationToken: 'Bearer deny' };
+        headers = { authorizationToken: 'Bearer deny', Authorization: "Bearer allow" };
         const response = await sendHttpRequest('GET', config.endPointURL, headers);
         expect(response.status).to.equal(403);
     } 
@@ -37,7 +37,7 @@ describe('API Gateway Authorization Tests', function() {
     }
   });
 
-  it('Should return 200 OK for authorizationToken header with value "Bearer allow"', async function() {
+  it('Return 200 OK for authorizationToken header with value "Bearer allow"', async function() {
     try {
         headers = { authorizationToken: 'Bearer allow' };
         const response = await sendHttpRequest('GET', config.endPointURL, headers);
@@ -48,7 +48,18 @@ describe('API Gateway Authorization Tests', function() {
     }
   });
 
-  it('Should return 500 Internal Server Error for authorizationToken header with any other value', async function() {
+  it('Return 200 OK for authorizationToken header with value "Bearer allow" and some additional headers', async function() {
+    try {
+        headers = { authorizationToken: 'Bearer allow', Authorization: "unauthorized", 'User-Agent': "bot traffic"};
+        const response = await sendHttpRequest('GET', config.endPointURL, headers);
+        expect(response.status).to.equal(200);
+    } 
+    catch (error) {
+        throw error;
+    }
+  });
+
+  it('Return 500 Internal Server Error for authorizationToken header with any other value', async function() {
     try {
         headers = { authorizationToken: 'randomValue' };
         const response = await sendHttpRequest('GET', config.endPointURL, headers);
